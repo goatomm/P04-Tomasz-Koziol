dda81b38b3afad15c474bdadea6cfe95d02c5ec4
goatomm <67665292+goatomm@users.noreply.github.com>
2020-12-04 20:48:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,15 @@
 
 using namespace std;
 //funkcja zczytujaca dane za pomoca wektora
-vector<int> read_data(istream& in, int n)
+vector<int> read_data(istream& in, int n)// funkcja zczytujaca wektor
 {
-    vector<int> ret;
+    vector<int> ret;//tworzenie pomocniczego wektora ret
 
-    for(int i = 0; i < n || n < 0; i++)
+    for(int i = 0; i < n || n < 0; i++) // petla zaczytujaca elementy wektora
     {
         int x;
-        if (in >> x)
-            ret.push_back(x);
-        else
+        if (in >> x) // wczytywanie wlementow do wekotra
+            ret.push_back(x);i<n // zwiekszanie konca wektora elementem x
             break;
     }
     return ret;
@@ -24,23 +23,23 @@ vector<int> read_data(istream& in, int n)
 //funkcja sortujaca wektor
 vector<int> sort_by_first_appearance(const vector<int>& vec)
 {
-    vector<int> ret = vec;
+    vector<int> ret = vec; // przypisanie wektora do zmiennej ret
 
-    map<int, size_t> first_seen;
+    map<int, size_t> first_seen; // tworzenie mapy ktora bedzie pamietala pierwsze wystapienia elementow
     size_t idx{};
     for(auto const& element : vec)
     {
-        first_seen.insert({element, idx++});
+        first_seen.insert({element, idx++}); // prawidlowe przypisanie pierwszego elementu
     }
 
-    sort(begin(ret), end(ret), [&](auto const& l, auto const& r)
+    sort(begin(ret), end(ret), [&](auto const& l, auto const& r) // posortowanie wedlug wystapeinia pierwszego elementu za pomoca funkcji sort zawartej w bibliotece algorithm
     {
-        return first_seen[l] < first_seen[r];
+        return first_seen[l] < first_seen[r]; // zwracanie posortowania
     });
-    return ret;
+    return ret; //zwraca prawidlowy wynik
 }
 //funkcja wyswietlajaca wektor
-void display_vec(const vector<int>& vec)
+void display_vec(const vector<int>& vec) // funckja wyswietlajaca wektor
 {
   for(int v : vec)
     {
@@ -48,7 +47,7 @@ void display_vec(const vector<int>& vec)
     }
 }
 //funkcja sprawdzajaca poprawnosc algorytmu liczb ponizej
-void run_tests()
+void run_tests() // fucnkja sprawdzajaca liczby
 {
     vector<int> a = {4,2,4,11,4,5,2,3,1,2,1};
     vector<int> b = {4,4,4,2,2,2,11,5,3,1,1};
@@ -64,12 +63,12 @@ void run_tests()
     cout << "Oczekiwano: " << endl;
     display_vec(b);
     cout << endl;
-    cout << "Wynik: " << boolalpha << (a_sorted == b) << endl;
+    cout << "Wynik: " << boolalpha << (a_sorted == b) << endl; // sprawdza czy "posortowany" wektor zgadza sie z wprowadzanym wektorem testowanym wektorem. jesli tak wypisze true, w przeciwnym wypadku false
 }
 
 int main()
 {
-    ofstream plik("wyniki.txt");
+    ofstream plik("wyniki.txt"); // deklarowanie pliku
     char choice;
     cout << "Wybierz tryb:" << endl;
     cout << " 1. Liczby z klawiatury" << endl;
@@ -79,7 +78,7 @@ int main()
     vector<int> nums;
 
     choice=getch();
-    switch(choice)
+    switch(choice) // switch ktory pozwala wybrac co mozemy zrobic
     {
     case '1':
     {
@@ -109,7 +108,7 @@ int main()
         return 0;
     }
     auto nums_sorted = sort_by_first_appearance(nums);
-    for (vector<int>::iterator it=nums_sorted.begin(); it!=nums_sorted.end(); it++)
+    for (vector<int>::iterator it=nums_sorted.begin(); it!=nums_sorted.end(); it++) // petla wypisujaca wektor do pliku
         plik<<' '<<*it;
     cout << "Do posortowania: " << endl;
     display_vec(nums);
